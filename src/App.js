6fa3995b935354d714410fbc278565d4ac617a14
OUAEL <benamaraouael1@gmail.com>
2024-03-01 11:01:15 +0100

    auth-edited

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './components/Home';
 import RequireAuth from './components/RequireAuth';
 
 import Profile from './components/Profile';
-import {theme} from './components/theme.js'
+import { theme } from './components/theme.js'
 import Main from './components/Main';
 import Inventory from './components/Inventory';
 import History from './components/History';
@@ -19,20 +19,30 @@ function App() {
     <ThemeProvider theme={theme}>
       <Box className="App" sx={{
         boxSizing: 'border-box',
-   
-        overflow:'hidden',
-        height:'100vh',
+
+        overflow: 'hidden',
+        height: '100vh',
       }} display={'flex'} flexDirection={'column'} >
         <Router>
           <Routes>
 
             <Route path='/' element={<Home />} />
-           
+
             <Route element={<RequireAuth />}>
-              <Route path='/main' element={<><Navbar /><Main/></>} />
-              <Route path='/inventory' element={<><Navbar /><Inventory/></>} />
+              <Route path='/main' element={<><Navbar /><Main /></>} />
+            </Route>
+            <Route element={<RequireAuth />}>
+
+              <Route path='/inventory' element={<><Navbar /><Inventory /></>} />
+            </Route>
+            <Route element={<RequireAuth />}>
+
               <Route path='/profile' element={<><Navbar /><Profile /></>} />
+            </Route>
+            <Route element={<RequireAuth />}>
+
               <Route path='/history' element={<><Navbar /><History /></>} />
+
             </Route>
 
           </Routes>
